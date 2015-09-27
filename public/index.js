159d8a63c4a692fbf28281d6413a76f9db805725
Gleb Gladky <gleba@yandex.ru>
2015-09-28 00:19:52 +0300

    SUCCESS

--- public/index.js
+++ public/index.js
@@ -1,283 +1,31 @@
 var app = angular.module("App", ['ui.router']);
 
+var lessons = [];
+
+$.getJSON( "/api/lessons", function( data ) {
+  lessons = data;
+});
+
 app.config(function ($stateProvider) {
-        $stateProvider.
-                state('index', {
-                    url: '',
-                    template: 'Go to <a href="#lessons">/lessons</a>',
-                    controller: 'MainController'
-                }).
-                state('lessons', {
-                    url: '/lessons',
-                    templateUrl: 'partials/lessons.html',
-                    controller: 'LessonsController'
-                })
-
-    });
-    
-app.controller('MainController', function($scope) {
+        $stateProvider
+          .state('index', {
+            url: '',
+            template: 'Go to <a href="#lessons">/lessons</a>'
+          })
+          .state('lessons', {
+            url: '/lessons',
+            templateUrl: 'partials/lessons.html',
+            controller: function($scope){
+              $scope.lessons = lessons;
+            }
+          })
+          .state('lesson', {
+            url: '/lessons/{lessonId}',
+            templateUrl: 'partials/lesson.html',
+            controller: function ($scope, $stateParams) {
+              $scope.title = lessons[$stateParams.lessonId - 1].title;
+              $scope.text = lessons[$stateParams.lessonId - 1].text;
+              $scope.faq = lessons[$stateParams.lessonId - 1].faq;
+            }
+          })
 });
-
-
-//$.getJSON( "/api/lessons", function( data ) {
-    var lessons = 
-[  
-   {  
-      "url":1,
-      "title":"Lesson 1 title",
-      "text":"Lesson 1 text",
-      "faq":[  
-         {  
-            "title":"Question 1 title",
-            "text":"Question 1 text"
-         },
-         {  
-            "title":"Question 2 title",
-            "text":"Question 2 text"
-         },
-         {  
-            "title":"Question 3 title",
-            "text":"Question 3 text"
-         },
-         {  
-            "title":"Question 4 title",
-            "text":"Question 4 text"
-         }
-      ]
-   },
-   {  
-      "url":2,
-      "title":"Lesson 2 title",
-      "text":"Lesson 2 text",
-      "faq":[  
-         {  
-            "title":"Question 1 title",
-            "text":"Question 1 text"
-         },
-         {  
-            "title":"Question 2 title",
-            "text":"Question 2 text"
-         },
-         {  
-            "title":"Question 3 title",
-            "text":"Question 3 text"
-         }
-      ]
-   },
-   {  
-      "url":3,
-      "title":"Lesson 3 title",
-      "text":"Lesson 3 text",
-      "faq":[  
-         {  
-            "title":"Question 1 title",
-            "text":"Question 1 text"
-         },
-         {  
-            "title":"Question 2 title",
-            "text":"Question 2 text"
-         },
-         {  
-            "title":"Question 3 title",
-            "text":"Question 3 text"
-         }
-      ]
-   },
-   {  
-      "url":4,
-      "title":"Lesson 4 title",
-      "text":"Lesson 4 text",
-      "faq":[  
-         {  
-            "title":"Question 1 title",
-            "text":"Question 1 text"
-         },
-         {  
-            "title":"Question 2 title",
-            "text":"Question 2 text"
-         },
-         {  
-            "title":"Question 3 title",
-            "text":"Question 3 text"
-         },
-         {  
-            "title":"Question 4 title",
-            "text":"Question 4 text"
-         }
-      ]
-   },
-   {  
-      "url":5,
-      "title":"Lesson 5 title",
-      "text":"Lesson 5 text",
-      "faq":[  
-         {  
-            "title":"Question 1 title",
-            "text":"Question 1 text"
-         },
-         {  
-            "title":"Question 2 title",
-            "text":"Question 2 text"
-         },
-         {  
-            "title":"Question 3 title",
-            "text":"Question 3 text"
-         },
-         {  
-            "title":"Question 4 title",
-            "text":"Question 4 text"
-         },
-         {  
-            "title":"Question 5 title",
-            "text":"Question 5 text"
-         }
-      ]
-   },
-   {  
-      "url":6,
-      "title":"Lesson 6 title",
-      "text":"Lesson 6 text",
-      "faq":[  
-         {  
-            "title":"Question 1 title",
-            "text":"Question 1 text"
-         },
-         {  
-            "title":"Question 2 title",
-            "text":"Question 2 text"
-         },
-         {  
-            "title":"Question 3 title",
-            "text":"Question 3 text"
-         }
-      ]
-   },
-   {  
-      "url":7,
-      "title":"Lesson 7 title",
-      "text":"Lesson 7 text",
-      "faq":[  
-         {  
-            "title":"Question 1 title",
-            "text":"Question 1 text"
-         },
-         {  
-            "title":"Question 2 title",
-            "text":"Question 2 text"
-         },
-         {  
-            "title":"Question 3 title",
-            "text":"Question 3 text"
-         },
-         {  
-            "title":"Question 4 title",
-            "text":"Question 4 text"
-         }
-      ]
-   },
-   {  
-      "url":8,
-      "title":"Lesson 8 title",
-      "text":"Lesson 8 text",
-      "faq":[  
-         {  
-            "title":"Question 1 title",
-            "text":"Question 1 text"
-         },
-         {  
-            "title":"Question 2 title",
-            "text":"Question 2 text"
-         },
-         {  
-            "title":"Question 3 title",
-            "text":"Question 3 text"
-         }
-      ]
-   },
-   {  
-      "url":9,
-      "title":"Lesson 9 title",
-      "text":"Lesson 9 text",
-      "faq":[  
-         {  
-            "title":"Question 1 title",
-            "text":"Question 1 text"
-         },
-         {  
-            "title":"Question 2 title",
-            "text":"Question 2 text"
-         },
-         {  
-            "title":"Question 3 title",
-            "text":"Question 3 text"
-         },
-         {  
-            "title":"Question 4 title",
-            "text":"Question 4 text"
-         },
-         {  
-            "title":"Question 5 title",
-            "text":"Question 5 text"
-         }
-      ]
-   },
-   {  
-      "url":10,
-      "title":"Lesson 10 title",
-      "text":"Lesson 10 text",
-      "faq":[  
-         {  
-            "title":"Question 1 title",
-            "text":"Question 1 text"
-         },
-         {  
-            "title":"Question 2 title",
-            "text":"Question 2 text"
-         },
-         {  
-            "title":"Question 3 title",
-            "text":"Question 3 text"
-         },
-         {  
-            "title":"Question 4 title",
-            "text":"Question 4 text"
-         },
-         {  
-            "title":"Question 5 title",
-            "text":"Question 5 text"
-         }
-      ]
-   }
-];
-
-app.controller('LessonsController', function($scope) {
-    $scope.lessons = lessons;
-});
-
-    
-
-for (var i = 1; i<=lessons.length; i++){
-    var stateName = 'lesson_' + i;
-    var stateUrl = '/lessons/'+ lessons[i-1].url;
-    var stateController = 'Lesson'+i+'Controller';
-
-    app.config(function ($stateProvider) {
-        $stateProvider.
-                state(stateName, {
-                    url: stateUrl,
-                    templateUrl: 'partials/lesson.html',
-                    controller: stateController
-                })
-    });
-
-    var title = lessons[i-1].title;
-    var text = lessons[i-1].text;
-    var faq = lessons[i-1].faq;
-    app.controller(stateController, function($scope) {
-        $scope.title = title;
-        $scope.text = text;
-        $scope.faq = faq;
-    });
-}
-
-//});
